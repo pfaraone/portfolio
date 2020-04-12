11353f547af90967aff73d2a46ed4cdcefb51876
Pierce <pf4tj@virginia.edu>
2020-04-12 16:32:02 +0800

    added pdf to gitignore, added social media links to own file, added resume as download

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,20 +3,23 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import myResume from "../assets/Pierce_Faraone_Resume.pdf"
 
 const resumePage = () => {
   return (
     <Layout>
       <SEO title="Resume" />
       <h1>Resume</h1>
-      <p>Post will show up here later on</p>
+      <a href={myResume} target="_blank" external button>
+        Download Resume
+      </a>
       &nbsp;
-      <div>
+      {/* <div>
         <Link to="/page-2/">Go to page 2</Link>
       </div>
       <div>
         <Link to="/page-3/">Go to page 3</Link>
-      </div>
+      </div> */}
     </Layout>
   )
 }
